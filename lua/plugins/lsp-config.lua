8c5ae042a788548e33c79b84824d54c8325047a2
Ali Malik <ali.m.malik@gmail.com>
2024-03-03 22:45:44 -0500

    modified lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,50 @@
 return {
-   { 
+    {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
         end
     },
-    { 
+    {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "html" }
+                ensure_installed = {
+                    "lua_ls",
+                    "ansiblels",
+                    "azure_pipelines_ls",
+                    "cssls",
+                    "html",
+                    "dockerls",
+                    "docker_compose_language_service",
+                    "tsserver",
+                    "pylsp",
+                    "tailwindcss",
+                    "templ",
+                    "terraformls",
+                }
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lsconfig = require("lspconfig")     
+            local lsconfig = require("lspconfig")
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities.textDocument.completion.completionItem.snippetSupport = true
-           
             lsconfig.lua_ls.setup({})
+            lsconfig.ansiblels.setup({})
+            lsconfig.azure_pipelines_ls.setup({})
+            lsconfig.cssls.setup({})
             lsconfig.html.setup({
                 capabilities = capabilities,
             })
+            lsconfig.dockerls.setup({})
+            lsconfig.tsserver.setup({})
+            lsconfig.pylsp.setup({})
+            lsconfig.tailwindcss.setup({})
+            lsconfig.templ.setup({})
+            lsconfig.terraformls.setup({})
 
             vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,31 @@
-return { 
-    "nvim-treesitter/nvim-treesitter", 
+return {
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            ensure_installed = { "go", "lua", "vim", "vimdoc", "html", "javascript", "python" },
+            ensure_installed = {
+                "go",
+                "lua",
+                "vim",
+                "vimdoc",
+                "html",
+                "javascript",
+                "css",
+                "csv",
+                "python",
+                "dockerfile",
+                "groovy",
+                "json",
+                "markdown",
+                "terraform",
+                "typescript",
+                "bash"
+            },
             highlight = { enable = true },
             indent = { enable = true }
         })
     end
 }
+
